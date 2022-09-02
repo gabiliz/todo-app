fa6890e87a9748e8cc5001a4d82d3702307efa20
gabiliz <gabrielalizmoreira@gmail.com>
2022-09-02 18:21:36 -0300

    feat: add tailwindscs and input

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { Input } from "./components/input";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>Todo list</h1>
+    <div className="flex flex-col items-center">
+      <h1 className="text-2xl font-bold">Todo list</h1>
+      <Input />
     </div>
+
   )
 }
 
---
+++ src/components/input/index.tsx
@@ -0,0 +1,5 @@
+export const Input = () => (
+  <form>
+    <input className="border-transparent rounded-lg bg-slate-200 focus:border-transparent w-96" type="text" placeholder="Write a new task" />
+  </form>
+);
